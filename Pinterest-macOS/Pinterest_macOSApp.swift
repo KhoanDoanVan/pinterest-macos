132cbb5e681d110c6381f13d193551403f6ffc27
khoanisdev <doanvankhoan124@gmail.com>
2025-05-14 23:12:31 +0700

    Initial Commit

---
+++ Pinterest-macOS/Pinterest_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinterest_macOSApp.swift
+//  Pinterest-macOS
+//
+//  Created by Đoàn Văn Khoan on 14/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Pinterest_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
